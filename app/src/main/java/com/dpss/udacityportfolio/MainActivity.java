78b1867318008fe7444cf0cdc1980e50fac57e8b
Jason Li <lizhansong@hotmail.com>
2017-01-04 20:18:01 +0800

    made changes per feedback of reviewer

--- app/src/main/java/com/dpss/udacityportfolio/MainActivity.java
+++ app/src/main/java/com/dpss/udacityportfolio/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,36 +19,37 @@ public class MainActivity extends AppCompatActivity {
     public void onButtonClick(View view){
         Log.d("onButtonClick","Hello, button:" + view.getId());
 
+        Button button = (Button)view;
         Context context = getApplicationContext();
-        String text = "Launching ";
+        String text = "Launching " + button.getText().toString();
 
-        switch(view.getId()){
-            case R.id.button:
-            {
-                text += "Popular Movies";
-                break;
-            }
-            case R.id.button2:
-            {
-                text += "Stock Hawk!";
-                break;
-            }
-            case R.id.button3:
-            {
-                text += "XYZ Reader";
-                break;
-            }
-            case R.id.button4:
-            {
-                text += "Material News";
-                break;
-            }
-            case R.id.button5:
-            {
-                text += "Final Project";
-                break;
-            }
-        }
+//        switch(view.getId()){
+//            case R.id.button:
+//            {
+//                text += "Popular Movies";
+//                break;
+//            }
+//            case R.id.button2:
+//            {
+//                text += "Stock Hawk!";
+//                break;
+//            }
+//            case R.id.button3:
+//            {
+//                text += "XYZ Reader";
+//                break;
+//            }
+//            case R.id.button4:
+//            {
+//                text += "Material News";
+//                break;
+//            }
+//            case R.id.button5:
+//            {
+//                text += "Final Project";
+//                break;
+//            }
+//        }
 
         //show toast
         Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
